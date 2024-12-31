fdb0d4d0288fd3ee5af71ba8b918dcc5b0e1da83
“DevHussein” <hussein_989@hotmail.com>
2024-12-31 19:55:37 +0300

    remove tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -8,13 +7,13 @@ export default defineConfig(({ isSsrBuild, command }) => ({
   build: {
     rollupOptions: isSsrBuild
       ? {
-          input: "./server/app.ts",
-        }
+        input: "./server/app.ts",
+      }
       : undefined,
   },
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [autoprefixer],
     },
   },
   ssr: {
